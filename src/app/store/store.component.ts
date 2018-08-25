5e2c957132da20ec74ae16042173890f76486dc3
calmosowl <calmol.owl@gmail.com>
2018-08-25 21:14:24 +0300

    Ch.8 Tbl.8.1

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
 
 @Component({
     selector: 'store',
-    moduleId: module.id,
+    // moduleId: module.id,
     templateUrl: 'store.component.html'
 })
 export class StoreComponent {
@@ -12,7 +13,8 @@ export class StoreComponent {
     public productsPerPage = 4;
     public selectedPage = 1;
 
-    constructor(private repository: ProductRepository) {}
+    constructor(private repository: ProductRepository,
+                private cart: Cart) {}
 
     get products(): Product[] {
         const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -42,9 +44,7 @@ export class StoreComponent {
             .getProducts(this.selectedCategory).length / this.productsPerPage)
     }
 
-    //get pageNumbers(): number[] {
-    //    return Array(Math.ceil(this.repository
-    //        .getProducts(this.selectedCategory).length / this.productsPerPage))
-    //            .fill(0).map((x, i) => i + 1);
-    //}
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
+    }
 }
